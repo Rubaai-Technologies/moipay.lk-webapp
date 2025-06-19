a37d82f2e28ce79888c7a15ebea052ad4b81d14c
Reganaselvaranjan <127473531+Reganaselvaranjan@users.noreply.github.com>
2025-06-19 12:40:07 +0530

    nav bar completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './screens/Home';
+import About from './screens/About';
+import Team from './screens/Team';
+import Contact from './screens/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="w-full min-h-screen bg-[rgba(18,18,18,1)] flex flex-col">
+        <Navbar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/screens/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="flex flex-1 items-center justify-center h-[70vh]">
+      <h1 className="text-3xl font-bold text-center">About Us</h1>
+    </div>
+  );
+} 
